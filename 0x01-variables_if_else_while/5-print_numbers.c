a5de69d933ec41231e143904f7c4223a941c59ee
rotichfrank4435 <rotichfrank3@gmail.com>
2023-09-10 13:04:52 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints singler digit of base 10 starting from 0
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
