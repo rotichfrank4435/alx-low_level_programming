1f4a534ce808bde7af8e7a7889afef4d75b46d8d
rotichfrank4435 <rotichfrank3@gmail.com>
2023-09-12 10:22:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,15 +3,15 @@
 #include <time.h>
 
 /**
- *  main - Entry point
+ * main - Entry point
  *
- *   Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 	printf("_putchar\n");
-
+	
 	return (0);
-
 }
+
