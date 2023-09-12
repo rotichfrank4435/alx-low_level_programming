d0c719e13348e76597fb87990951046e8c5577c7
rotichfrank4435 <rotichfrank3@gmail.com>
2023-09-12 11:52:10 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks
+ * @c: be allowed
+ * Return: 1 if is a letter otherwise 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c < 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}	
